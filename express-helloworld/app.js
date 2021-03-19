2c15a300611ae6b603581ff53d3703fdf3c8fd08
Andi Sugandi <andisugandi@gmail.com>
2021-03-19 10:07:01 +0700

    Add /julia endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/julia', function (req, res) {
+  res.send('<h2>Hello, Kanaya!</h2>\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
